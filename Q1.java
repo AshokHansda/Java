f49babf0c31e192dfc4ebf814759b0030af3b3a6
Anjali Sharma <131297940+itssanjali@users.noreply.github.com>
2023-04-20 02:01:51 +0530

    Updated ❤️

--- Q1.java
+++ Q1.java
@@ -2,26 +2,14 @@ import java.util.Scanner;
 import java.util.TreeSet;
 public class Q1 {
     public static void main(String[] args) {
-        Scanner scanner = new Scanner(System.in);
-        TreeSet<Integer> numbers = new TreeSet<>();
-        numbers.add(10);
-        numbers.add(20);
-        numbers.add(30);
-        numbers.add(40);
-        System.out.println("TreeSet: " + numbers);
-        System.out.print("Enter a number to search: ");
-        int searchNumber = scanner.nextInt();
-        boolean isPresent = numbers.contains(searchNumber);
-        System.out.println("Number " + searchNumber + " is" + (isPresent ? " " : " not ") + "present in the list");
-        System.out.print("Enter an element to remove: ");
-        int removeNumber = scanner.nextInt();
-        boolean isRemoved = numbers.remove(removeNumber);
-        System.out.println("Number " + removeNumber + (isRemoved ? " was " : " was not ") + "removed from the list");
-        System.out.println("Updated TreeSet: " + numbers);
+        
         LinkedHashSet<Double> doubleSet = new LinkedHashSet<>();
         doubleSet.add(3.14);
         doubleSet.add(2.718);
         doubleSet.add(1.618);
+        doubleSet.add(2.718);
+        doubleSet.add(2.718);
+        doubleSet.add(2.718);
         System.out.println("LinkedHashSet: " + doubleSet);
         LinkedHashSet<Double> doubleSet = new LinkedHashSet<>();
         doubleSet.add(3.14);
